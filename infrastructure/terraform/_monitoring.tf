3938907b680d11298a031f0a9aee18dfdec0cea1
Goran Grujic <goran.grujic@prodyna.com>
2020-07-20 22:16:20 +0200

    revert

--- infrastructure/terraform/_monitoring.tf
+++ infrastructure/terraform/_monitoring.tf
@@ -15,13 +15,4 @@ resource "helm_release" "prometheus-operator" {
   ]
 }
 
-resource "kubernetes_secret" "prometheus-basic-auth" {
-  metadata {
-    name = "prometheus-basic-auth"
-    namespace = kubernetes_namespace.monitoring.metadata[0].name
-  }
-
-  data = {
-    "auth" = "prometheus:$apr1$BW1Uu32h$Ru5z2hKVnfHNSPa4y2CT41"
-  }
 }
