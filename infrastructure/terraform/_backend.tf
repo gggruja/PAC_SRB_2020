fef3db42d3c626ecbe43321f86d2b39e58489ca7
Goran Grujic <goran.grujic@prodyna.com>
2020-08-03 12:59:47 +0200

    Adding backend as a helm chart

--- infrastructure/terraform/_backend.tf
+++ infrastructure/terraform/_backend.tf
@@ -3,3 +3,14 @@ resource "kubernetes_namespace" "backend" {
     name = "backend"
   }
 }
+
+
+resource "helm_release" "backend" {
+  name = "backend"
+  chart = "../chart/backend"
+  namespace = kubernetes_namespace.backend.metadata[0].name
+
+  values = [
+    file("helm/backend.yaml")
+  ]
+}
