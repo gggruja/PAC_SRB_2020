43bf4a58168625b283cada7e39a229e64661281e
Goran Grujic <goran.grujic@prodyna.com>
2020-08-03 18:09:34 +0200

    Addong GORM

--- backend/main.go
+++ backend/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gggruja/PAC_SRB_2020/backend/config"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
+	"github.com/jinzhu/gorm"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 )
 
-var db *sql.DB
+var db *gorm.DB
 
 func healthChecking(rw http.ResponseWriter, r *http.Request) {
 	log.Println("Healthy App - Running!")
@@ -26,6 +27,10 @@ func healthChecking(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "text/plain")
 	rw.WriteHeader(http.StatusOK)
 	rw.Write([]byte("Backend App - Goran Grujic!"))
+}
+
+// initialize DB
+func initDB() {
 }
 
 func main() {
@@ -42,7 +47,7 @@ func main() {
 
 	fmt.Println("Go with MariaDB")
 
-	db, err = sql.Open("mysql", cnf.BindDatabase)
+	db, err = gorm.Open("mysql", cnf.BindDatabase)
 
 	if err != nil {
 		panic(err.Error())
@@ -50,12 +55,23 @@ func main() {
 
 	defer db.Close()
 
+	// Migrate the schema
+	db.AutoMigrate(&Location{})
+
 	sm := mux.NewRouter()
 
 	sm.HandleFunc("/", healthChecking)
 	sm.Handle("/metrics", promhttp.Handler())
-	sm.HandleFunc("/locations", GetLocations).Methods("GET")
-	sm.HandleFunc("/location", PostLocations).Methods("POST")
+
+	//LOCATIONS API's
+	sm.HandleFunc("/locations", createLocations).Methods("POST")
+	sm.HandleFunc("/locations", getLocations).Methods("GET")
+	sm.HandleFunc("/locations/{locationId}", getLocation).Methods("GET")
+	sm.HandleFunc("/locations/{locationId}", updateLocation).Methods("PUT")
+	sm.HandleFunc("/locations/{locationId}", deleteLocation).Methods("DELETE")
+
+	// Initialize db connection
+	initDB()
 
 	// create Server
 	s := http.Server{
@@ -92,58 +108,63 @@ func main() {
 }
 
 type Location struct {
-	Location_id   int    `json:"id"`
-	Location_name string `json:"location_name"`
+	gorm.Model
+	LocationName string `json:"location_name"`
 }
 
-func GetLocations(w http.ResponseWriter, r *http.Request) {
+func deleteLocation(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	inputLocationId := params["locationId"]
+	// Convert `locationId` string param to uint64
+	id64, _ := strconv.ParseUint(inputLocationId, 10, 64)
+	// Convert uint64 to uint
+	idToDelete := uint(id64)
+
+	db.Where("id = ?", idToDelete).Delete(&Location{})
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateLocation(w http.ResponseWriter, r *http.Request) {
+
+	var updatedLocation Location
+	json.NewDecoder(r.Body).Decode(&updatedLocation)
+	db.Save(&updatedLocation)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedLocation)
+
+}
+
+func getLocation(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	inputLocationId := params["locationId"]
+
+	var location Location
+	db.First(&location, inputLocationId)
+	json.NewEncoder(w).Encode(location)
+
+}
+
+func getLocations(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
 	var locations []Location
-
-	result, err := db.Query("select Location_id,Location_name from Location")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer result.Close()
-
-	for result.Next() {
-		var location Location
-		err := result.Scan(&location.Location_id, &location.Location_name)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		locations = append(locations, location)
-
-	}
-
+	db.Find(&locations)
 	json.NewEncoder(w).Encode(locations)
 
 }
 
-func PostLocations(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
+func createLocations(w http.ResponseWriter, r *http.Request) {
 
 	var location Location
+	json.NewDecoder(r.Body).Decode(&location)
 
-	_ = json.NewDecoder(r.Body).Decode(&location)
-
-	stmt, es := db.Prepare("insert into Location values(?,?)")
-	if es != nil {
-		panic(es.Error())
-	}
-
-	_, er := stmt.Exec(location.Location_id, location.Location_name)
-	if er != nil {
-		panic(er.Error())
-	}
-
+	db.Create(&location)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(location)
 
 }
