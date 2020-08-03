ae832b2f7a5569f3f56e6592bd8c4d428810f00e
Goran Grujic <goran.grujic@prodyna.com>
2020-08-03 15:23:43 +0200

    Adding get from database

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
 	"github.com/gggruja/PAC_SRB_2020/backend/config"
@@ -11,7 +13,11 @@ import (
 	"os"
 	"os/signal"
 	"time"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
 
 func healthChecking(rw http.ResponseWriter, r *http.Request) {
 	log.Println("Healthy App - Running!")
@@ -34,10 +40,21 @@ func main() {
 	}
 	l.Info("Loaded config: ", hclog.Fmt("%+v", cnf))
 
+	fmt.Println("Go with MariaDB")
+
+	db, err = sql.Open("mysql", "keycloak:kcdgXeyiHwC1bJcb@tcp(127.0.0.1:3306)/keycloak")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
 	sm := mux.NewRouter()
 
 	sm.HandleFunc("/", healthChecking)
 	sm.Handle("/metrics", promhttp.Handler())
+	sm.HandleFunc("/locations", GetLocations).Methods("GET")
 
 	// create Server
 	s := http.Server{
@@ -72,3 +89,40 @@ func main() {
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(ctx)
 }
+
+type Location struct {
+	Location_id   int    `json:"id"`
+	Location_name string `json:"location_name"`
+}
+
+func GetLocations(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var webs []Location
+
+	result, err := db.Query("select Location_id,Location_name from Location")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+		var website Location
+		err := result.Scan(&website.Location_id, &website.Location_name)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		webs = append(webs, website)
+
+	}
+
+	json.NewEncoder(w).Encode(webs)
+
+}
+
+
