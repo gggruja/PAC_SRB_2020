35e654960499c3b24d63c1725198e3aad56e13c3
Goran Grujic <goran.grujic@prodyna.com>
2020-08-11 15:00:38 +0200

    Refactoring and creating view that works locally

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,71 +1,18 @@
 <template>
     <div id="app">
-        <h1>Conference APP</h1>
-        <button v-on:click="getLocations">Get Events</button>
-        <button v-on:click="getPeople">Get People</button>
-        <button v-on:click="getTopics">Get Topics</button>
-        <button v-on:click="getOverview">Get Overview</button>
+        <Events>
 
-        <Locations locations="locations">
-
-        </Locations>
-
-        <Talk>
-
-        </Talk>
-        <Person>
-
-        </Person>
-
-        <Overview>
-
-        </Overview>
+        </Events>
     </div>
 </template>
 
 <script>
-    import Locations from "./components/Locations";
-    import Talk from "./components/Talk";
-    import Person from "./components/Person";
-    import Overview from "./components/Overview";
+    import Events from "./components/Events";
 
     export default {
         name: 'App',
         components: {
-            Locations,
-            Talk,
-            Person,
-            Overview
-        },
-        data() {
-            return {
-                locations: [],
-                people: [],
-                topics: [],
-                overview: []
-            };
-        },
-        methods: {
-            getLocations() {
-                fetch("http://conference.backend/locations")
-                    .then(response => response.json())
-                    .then(data => (this.locations = data));
-            },
-            getPeople() {
-                fetch("http://conference.backend/persons")
-                    .then(response => response.json())
-                    .then(data => (this.people = data));
-            },
-            getTopics() {
-                fetch("http://conference.backend/topics")
-                    .then(response => response.json())
-                    .then(data => (this.topics = data));
-            },
-            getOverview() {
-                fetch("http://conference.backend/topics")
-                    .then(response => response.json())
-                    .then(data => (this.overview = data));
-            }
+            Events
         }
     }
 </script>
---
+++ frontend/src/components/Events.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Conference APP</h1>
+        <button v-on:click="getEvents">Get Events</button>
+        <table style="width:100%">
+            <thead>
+            <tr>
+                <th>Event Name</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Location Name</th>
+                <th>Room Name</th>
+                <th>Talk Title</th>
+                <th>Topic Name</th>
+            </tr>
+            </thead>
+            <tbody v-for="event in events" :key="event.ID">
+            <tr>
+                <td>{{event.EventName}}</td>
+                <td>{{event.StartDate}}</td>
+                <td>{{event.EndDate}}</td>
+                <td>{{event.LocationName}}</td>
+                <td>{{event.RoomName}}</td>
+                <td>{{event.TitleName}}</td>
+                <td>{{event.TopicName}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Events",
+        data() {
+            return {
+                events: []
+            };
+        },
+        methods: {
+            getEvents() {
+                fetch("http://conference.backend/api/events")
+                    .then(response => response.json())
+                    .then(data => (this.events = data));
+            }
+        }
+    };
+</script>
