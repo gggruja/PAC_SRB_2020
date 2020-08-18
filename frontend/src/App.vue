dd12f260b82f4b0149f94c451d77021d48ed39c1
Goran Grujic <goran.grujic@prodyna.com>
2020-08-18 19:24:45 +0200

    remove conference app h1 to app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="app">
+        <h1>Conference APP</h1>
         <Events>
 
         </Events>
+
     </div>
 </template>
 
--- frontend/src/components/Events.vue
+++ frontend/src/components/Events.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h1>Conference APP</h1>
         <button v-on:click="getEvents">Get Events</button>
         <table style="width:100%">
             <thead>
