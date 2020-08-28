3de578a3854f490522c311e0e15437f193423abd
Goran Grujic <goran.grujic@prodyna.com>
2020-08-28 14:31:59 +0200

    adding grafana dashboard automaticly

--- infrastructure/terraform/_monitoring.tf
+++ infrastructure/terraform/_monitoring.tf
@@ -25,3 +25,17 @@ resource "kubernetes_secret" "prometheus-basic-auth" {
     "auth" = "prometheus:$apr1$BW1Uu32h$Ru5z2hKVnfHNSPa4y2CT41"
   }
 }
+
+resource "kubernetes_config_map" "grafana_dashboard" {
+  metadata {
+    name = "grafana_dashboard"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      "grafana_dashboard" : "1"
+    }
+  }
+
+  data = {
+    "backend-app.json" = file("dashboard/grafana/backend-app.json")
+  }
+}
